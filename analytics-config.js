5f99723d3c757d0fc0c434ab9ecec4e2d4075204
praise-lab <54062614+zhoufenze@users.noreply.github.com>
2026-05-08 19:11:53 +0800

    Fix PostHog project token

--- analytics-config.js
+++ analytics-config.js
@@ -1,7 +1,7 @@
 window.DAILY_PRAISE_ANALYTICS = {
   posthog: {
     enabled: true,
-    projectApiKey: "phc_r9ZKKZwXH3BaPtPThSjJNF3tGy7pFokXmBCYNzQzpMkc",
+    projectApiKey: "phc_r9ZKkZwxH3BaPtPThSjJNF3tGy7pFokXmBCYNzQzpMkc",
     apiHost: "https://us.i.posthog.com"
   }
 };
